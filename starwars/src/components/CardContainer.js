8977bd8dec264928928c183a75316a197e350dd3
unknown <jacob.e.england@gmail.com>
2020-02-21 11:42:02 -0600

    State is correctly updating

--- starwars/src/components/CardContainer.js
+++ starwars/src/components/CardContainer.js
@@ -9,20 +9,34 @@ const CardContainer = () => {
     const [homeworlds, setHomeworlds] = useState([]);
     const [films, setFilms] = useState([]);
     const [species, setSpecies] = useState([]);
+    // Axios call to get character data
     useEffect(() => {
         axios.get(`https://swapi.co/api/people/?page=${page}`)
             .then(response => setData(response.data.results))
     },[page])
+    // Axios calls to get data stored at inner API locations
     useEffect(() => {
         if (data.length > 0) {
-            let homes = [];
+            // Had to use multiple maps or the code would break for some reason
             data.map((character) => (
                 axios.get(character.homeworld)
                     .then(response => setHomeworlds(homeworlds => [...homeworlds, response.data.name]))
             ))
+            data.map((character) => {
+                axios.get(character.species)
+                    .then(response => setSpecies(species => [...species, response.data.name]));
+            })
+            data.map((character => {
+                let filmList = [];
+                character.films.map((film) => {
+                    axios.get(film)
+                        .then(response => filmList.push(response.data.title));
+                })
+                setFilms(films => [...films, filmList])
+            }))
         }
-    },[data])
-    console.log(homeworlds);
+    }, [data])
+    console.log(films)
     return (
         <div>
             Hello World
